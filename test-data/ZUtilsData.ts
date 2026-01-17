0e198ee10bbac068da39980c83e00fc615bb7b16
CDTester <chris_a_day@hotmail.com>
2026-01-17 15:14:53 +0000

    utilsData

---
+++ test-data/ZUtilsData.ts
@@ -0,0 +1,9 @@
+export class ZUtilsData {
+  static generateRandomEmail(): string {
+    return `test${Date.now()}@example.com`;
+  }
+
+  static generateRandomString(length: number = 10): string {
+    return Math.random().toString(36).substring(2, length + 2);
+  }
+}
